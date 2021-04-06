50f63dc0831b7ef24a56cc43fa4a35dc266eeba7
p2terkim <p2terkim@gmail.com>
2021-04-06 20:25:38 +0800

    고급자바1일

--- collectionTest/src/kr/or/ddit/basic/ArrayListTest04.java
+++ collectionTest/src/kr/or/ddit/basic/ArrayListTest04.java
@@ -2,6 +2,11 @@ package kr.or.ddit.basic;
 
 public class ArrayListTest04 {
 
+	/*
+
+ 		
+ 	문제2) 문제1에서 별명의 길이가 같은 것이 있을 경우를 처리하시오
+ */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
